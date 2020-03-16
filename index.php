9fb82ddce40dd9a3b7fbd342b0fa76e0ea38e494
jesus leon ortiz <leon-herido1@hotmail.com>
2020-03-15 23:52:32 -0600

    repisitorio inicial

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+  <head>
+    <title>A-Frame Blender Projects</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.1.1/gh-fork-ribbon.min.css"/>
+    <style>
+      html {
+        background: rgb(15, 15, 15);
+        color: #FAFAFA;
+        font-family: monospace;
+        font-size: 20px;
+        padding: 10px 20px;
+      }
+      h1 {
+        font-weight: 300;
+      }
+      #container {
+        display: flex;
+        list-style: none;
+        flex-wrap: wrap;
+      }
+      a {
+        display: block;
+        padding: 15px 0;
+      }
+      img  {
+        max-height: 480px;
+        max-width: 480px;
+      }
+    </style>
+  </head>
+  <body>
+   <center> <h1>proyecto tortuga </h1></center>
+
+    <ul id="container">
+      <li><a href="cup/index.html"><img src="img/torta.png"></a></li>
+    </ul>
+
+    <div class="github-fork-ribbon-wrapper right">
+      <div class="github-fork-ribbon" style="background: rgb(12, 12, 12)">
+        <a href="https://github.com/ngokevin/aframe-blender-projects">Fork me on GitHub</a>
+      </div>
+    </div>
+  </body>
+</html>
